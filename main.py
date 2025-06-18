f66611ab928bdd0e653190c8402aa21738f4a2fa
mehmat007 <97409984+mehmat007@users.noreply.github.com>
2025-06-18 09:21:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # main.py
 
+"""
+This module contains a simple greeting function.
+"""
+
 def say_hello(name):
+    """
+    Print a greeting to the specified name.
+
+    Args:
+        name (str): The name of the person to greet.
+    """
     print(f"Hello, {name}!")
 
 
